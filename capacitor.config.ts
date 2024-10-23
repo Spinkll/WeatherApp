f38db00cdc470512eccf652439157a07d8124086
Spinkll <roma.skorohod.2005@gmail.com>
2024-10-23 13:59:43 +0300

    fix gps

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'quasarweatherapp',
-  webDir: 'dist/spa'
+  webDir: 'dist/spa',
+  plugins: {
+    Geolocation: {
+      permissions: [
+        "ACCESS_FINE_LOCATION",
+        "ACCESS_COARSE_LOCATION"
+      ]
+    }
+  }
 };
 
 export default config;
